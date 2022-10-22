05c1a36fb09afd69d9d92998fd4388f48d0cda5b
Dylan Howell <dylanleehowell@gmail.com>
2022-10-21 22:19:25 -0500

    Update display of Ranks

--- src/model/enums.rs
+++ src/model/enums.rs
@@ -17,18 +17,7 @@ pub enum PlayerName {
     FOE,
 }
 
-#[derive(
-    Debug,
-    Hash,
-    Eq,
-    PartialEq,
-    strum_macros::EnumIter,
-    strum_macros::Display,
-    Copy,
-    Clone,
-    Ord,
-    PartialOrd,
-)]
+#[derive(Debug, Hash, Eq, PartialEq, strum_macros::EnumIter, Copy, Clone, Ord, PartialOrd)]
 pub enum Rank {
     ACE,
     TWO,
@@ -45,6 +34,27 @@ pub enum Rank {
     KING,
 }
 
+impl Display for Rank {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let symbol = match self {
+            Rank::ACE => "A",
+            Rank::TWO => "2",
+            Rank::THREE => "3",
+            Rank::FOUR => "4",
+            Rank::FIVE => "5",
+            Rank::SIX => "6",
+            Rank::SEVEN => "7",
+            Rank::EIGHT => "8",
+            Rank::NINE => "9",
+            Rank::TEN => "10",
+            Rank::JACK => "J",
+            Rank::QUEEN => "Q",
+            Rank::KING => "K",
+        };
+        write!(f, "{symbol}")
+    }
+}
+
 #[derive(Debug, strum_macros::EnumIter, Copy, Clone, Eq, PartialEq, Hash)]
 pub enum Suit {
     HEARTS,
@@ -56,7 +66,6 @@ pub enum Suit {
 impl Display for Suit {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         let symbol = match self {
-            //  (♣), diamonds (♦), hearts (♥), and spades (♠).
             Suit::CLUBS => "♣",
             Suit::DIAMONDS => "♦",
             Suit::HEARTS => "♥",
